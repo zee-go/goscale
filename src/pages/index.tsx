07e6ebddc42a05748076b417d73ea2f27d92264f
softgenai <admin@softgen.ai>
2025-11-24 19:04:53 +0000

    Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,21 @@ import { HeroSection } from "@/components/HeroSection";
 import { PortfolioSection } from "@/components/PortfolioSection";
 import { ValuePropositionSection } from "@/components/ValuePropositionSection";
 import { ServicesSection } from "@/components/ServicesSection";
+import { PlatformsSection } from "@/components/PlatformsSection";
 import { ResultsSection } from "@/components/ResultsSection";
 import { ContactSection } from "@/components/ContactSection";
 import { Footer } from "@/components/Footer";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-white">
       <Header />
       <main>
         <HeroSection />
         <PortfolioSection />
         <ValuePropositionSection />
         <ServicesSection />
+        <PlatformsSection />
         <ResultsSection />
         <ContactSection />
       </main>
